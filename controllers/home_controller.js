c31ed3a37d0003bf4b1f668d30a8b35ba5cbdd3f
Rajat Naraini <rajat2985@gmail.com>
2021-09-25 01:41:28 +0530

    set up view and created couple of them also added ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,14 @@
 module.exports.home = (req, res) => {
-    return res.end('<h1>Express is up for Codeial</h1>')
+    // return res.end('<h1>Express is up for Codeial</h1>')
+    return res.render('home',{
+        title: "Home"
+    })
 }
 module.exports.anotherhome = (req, res) => {
-    return res.end('<h1>Another Home Reached</h1>')
+    // return res.end('<h1>Another Home Reached</h1>')
+    return res.render('home',{
+        title: "Another Home"
+    })
 }
 
 // module.exports.actionname = (req, res) {}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,9 @@
 module.exports.profile = (req, res) => {
-    res.end('<h1>User Profile</h1>')
+    // res.end('<h1>User Profile</h1>')
+    res.render('user',{
+        name: 'Rajat',
+        contact: 9501345915
+    })
 }
 module.exports.salaryDetails = (req, res) => {
     res.end('<h1>Salary Details</h1>')
